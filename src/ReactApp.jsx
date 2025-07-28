11f8ddbce53ca0602d67b4ff21ef6a83de6db698
mofey <mofey@outlook.com>
2025-07-28 17:09:05 +0100

    updated the landing page

--- src/ReactApp.jsx
+++ src/ReactApp.jsx
@@ -1,27 +1,10 @@
 import logo from './logo.svg';
 import { Outlet, Link } from "react-router-dom";
+import Movietent from './Movietent'
 
 function ReactApp() {
-  const name = "React"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link to="/Movietent">Hello {name}</Link>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Outlet />
-    </div>
+    <Movietent />
   );
 }
 
